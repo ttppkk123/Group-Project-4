0a9b7507a5e659f4becc6d2a9fb604a40676ce66
ttppkk123 <62799607+ttppkk123@users.noreply.github.com>
2026-06-12 18:04:47 +0200

    Add files via upload

---
+++ contact_success.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Success</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/contact_success.css">
+</head>
+<body>
+
+    <div class="success_container">
+        <?php
+        echo "<h1>Your form is submitted.</h1>";
+        echo "<a href='index.php'>Back</a>";
+        ?>
+    </div>
+</body>
+</html>
---
+++ data.php
@@ -0,0 +1,158 @@
+<?php
+
+$nav = [
+    ["title" => "Home", "url" => "index.php", "active" => true],
+    ["title" => "About", "url" => "about.php", "active" => false],
+    ["title" => "Treatment", "url" => "treatment.php", "active" => false],
+    ["title" => "Doctors", "url" => "doctor.php", "active" => false],
+    ["title" => "Testimonial", "url" => "testimonial.php", "active" => false],
+    ["title" => "Contact Us", "url" => "contact.php", "active" => false]
+];
+
+$slider = [
+    [
+        "title" => "Mico Hospital",
+        "highlight" => "Hospital",
+        "text" => "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to",
+        "img" => "images/slider-img.jpg",
+        "button" => "Contact Us",
+        "link" => "contact.php"
+    ],
+    [
+        "title" => "Best Care",
+        "highlight" => "Service",
+        "text" => "High quality medical service with modern equipment and professional staff.",
+        "img" => "images/slider-img.jpg",
+        "button" => "Contact Us",
+        "link" => "contact.php"
+    ],
+    [
+        "title" => "Trusted Doctors",
+        "highlight" => "Clinic",
+        "text" => "We provide trusted healthcare solutions for all patients.",
+        "img" => "images/slider-img.jpg",
+        "button" => "Contact Us",
+        "link" => "contact.php"
+    ]
+];
+
+$treatments = [
+    [
+        "img" => "images/t1.png",
+        "title" => "Nephrologist Care",
+        "desc" => "alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+        "link" => "treatment.php?id=1"
+    ],
+    [
+        "img" => "images/t2.png",
+        "title" => "Eye Care",
+        "desc" => "alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+        "link" => "treatment.php?id=2"
+    ],
+    [
+        "img" => "images/t3.png",
+        "title" => "Pediatrician Clinic",
+        "desc" => "alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+        "link" => "treatment.php?id=3"
+    ],
+    [
+        "img" => "images/t4.png",
+        "title" => "Parental Care",
+        "desc" => "alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything",
+        "link" => "treatment.php?id=4"
+    ]
+];
+
+$doctors = [
+    [
+        "name" => "Hennry",
+        "spec" => "MBBS",
+        "img" => "images/team1.jpg",
+        "facebook" => "#",
+        "twitter" => "#",
+        "linkedin" => "#",
+        "instagram" => "#"
+    ],
+    [
+        "name" => "Jenni",
+        "spec" => "MBBS",
+        "img" => "images/team2.jpg",
+        "facebook" => "#",
+        "twitter" => "#",
+        "linkedin" => "#",
+        "instagram" => "#"
+    ],
+    [
+        "name" => "Morco",
+        "spec" => "MBBS",
+        "img" => "images/team3.jpg",
+        "facebook" => "#",
+        "twitter" => "#",
+        "linkedin" => "#",
+        "instagram" => "#"
+    ]
+];
+
+$testimonials = [
+    [
+        "name" => "Morijorch",
+        "subtitle" => "Default model text",
+        "text" => "editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various"
+    ],
+    [
+        "name" => "Rochak",
+        "subtitle" => "Default model text",
+        "text" => "Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
+    ],
+    [
+        "name" => "Brad Johns",
+        "subtitle" => "Default model text",
+        "text" => "Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
+    ]
+];
+
+$site = [
+    "logo" => "images/logo.png",
+    "address" => "New Taipei, Taiwan",
+    "phone" => "+01 1234567890",
+    "email" => "demo@gmail.com",
+
+    "social" => [
+        "facebook" => "#",
+        "twitter" => "#",
+        "linkedin" => "#",
+        "instagram" => "#"
+    ]
+];
+
+$links = [
+    ["title" => "Home", "url" => "index.php"],
+    ["title" => "About", "url" => "about.php"],
+    ["title" => "Treatment", "url" => "treatment.php"],
+    ["title" => "Doctors", "url" => "doctor.php"],
+    ["title" => "Testimonial", "url" => "testimonial.php"],
+    ["title" => "Contact us", "url" => "contact.php"]
+];
+
+$posts = [
+    [
+        "img" => "images/post1.jpg",
+        "text" => "Normal distribution"
+    ],
+    [
+        "img" => "images/post2.jpg",
+        "text" => "Normal distribution"
+    ]
+];
+
+$news = [
+    [
+        "img" => "images/post3.jpg",
+        "text" => "Health news"
+    ],
+    [
+        "img" => "images/post4.png",
+        "text" => "Hospital update"
+    ]
+];
+
