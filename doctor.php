131795d8b2bebd9d1acef74b3d9218143341856a
ttppkk123 <62799607+ttppkk123@users.noreply.github.com>
2026-06-12 22:05:52 +0200

    Update doctor.php

--- doctor.php
+++ doctor.php
@@ -166,7 +166,7 @@
 
 
 
- <!-- info section -->
+  <!-- info section -->
   <section class="info_section ">
     <div class="container">
       <div class="info_top">
@@ -177,9 +177,9 @@
         </div>
         <div class="info_form">
           <form action="">
-            <input type="email" placeholder="Your email">
+            <input type="email" placeholder="თქვენი ელ-ფოსტა">
             <button>
-              Subscribe
+              გამოწერა
             </button>
           </form>
         </div>
@@ -188,7 +188,7 @@
         <div class="row info_main_row">
           <div class="col-md-6 col-lg-3">
             <h5>
-              Address
+              მისამართი
             </h5>
             <div class="info_contact">
               <a href="">
@@ -206,7 +206,7 @@
             </div>
             
             <!-- SOCIAL BOX -->
-            <div class="social_box">
+            <div class="სოციალური ქსელები">
               <a href="<?php echo $site['social']['facebook']; ?>">
                 <i class="fa fa-facebook" aria-hidden="true"></i>
               </a>
@@ -225,7 +225,7 @@
           <div class="col-md-6 col-lg-3">
             <div class="info_links">
               <h5>
-                Useful link
+                სასარგებლო ლინკები
               </h5>
               <div class="info_links_menu">
                 <?php foreach ($links as $link): ?>
@@ -240,7 +240,7 @@
           <div class="col-md-6 col-lg-3">
             <div class="info_post">
               <h5>
-                LATEST POSTS
+                უახლესი პოსტები
               </h5>
               <?php foreach ($latest_posts as $post): ?>
                 <div class="post_box">
@@ -257,7 +257,7 @@
           <div class="col-md-6 col-lg-3">
             <div class="info_post">
               <h5>
-                News
+                სიახლეები
               </h5>
               <?php foreach ($news as $post): ?>
                 <div class="post_box">
@@ -276,12 +276,11 @@
   <!-- end info_section -->
 
 
-
   <!-- footer section -->
   <footer class="footer_section">
     <div class="container">
       <p>
-        &copy; <span id="displayYear"></span> All Rights Reserved By
+        &copy; <span id="displayYear"></span> ყველა უფლება დაცულია
         <a href="https://html.design/">Free Html Templates</a>
       </p>
     </div>
