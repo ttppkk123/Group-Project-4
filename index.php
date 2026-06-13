796d4c55d2515ef7bfab3c74a7b4e23bdadeaacb
ttppkk123 <tpkhakadze08@gmail.com>
2026-06-13 11:36:00 +0200

    clean index

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
 <?php include 'data.php'; ?>
 <?php include 'main_page.php'; ?>
-<?php include 'about.php'; ?>
-<?php include 'contact.php'; ?>
-<?php include 'doctor.php'; ?>
-<?php include 'testimonial.php'; ?>
-<?php include 'treatment.php'; ?>
